a8d7b8eacc3846271f2d961d7ccb2249a1281740
Ramazan Özdemir <ramazanozdemir2607@gmail.com>
2023-10-03 23:00:58 +0200

    auth microservice is added

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,29 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const port = Number(process.env.AUTH_MS_PORT) || 4001;
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port,
+      },
+    }
+  );
+
+  await app.listen();
+  Logger.log(`🚀 Auth Micro service is running `);
+}
+
+bootstrap();
